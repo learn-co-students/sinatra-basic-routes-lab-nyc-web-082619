3f16b010f218244f7fea42f651d480e8a5dd2b4b
Tania Manco <belalov.tania16@gmail.com>
2019-09-16 12:51:52 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Tania"
+    end
+
+    get '/hometown' do 
+        "My hometown is Ashkelon"
+    end
+
+    get '/favorite-song' do
+     "My favorite song is Hello"
+    end 
+
 end
