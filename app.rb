ae4eef54f854f4a6ce9cca0cf97193fbe74353a6
Eugenia Kim <elk303@nyu.edu>
2019-09-15 22:54:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,27 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "hello world"
+    end
+
+    get '/name' do
+
+        "My name is __"
+
+    end
+
+    get '/hometown' do
+
+        "My hometown is __"
+
+    end
+
+    get '/favorite-song' do
+
+        "My favorite song is ___"
+
+    end
+
 end
